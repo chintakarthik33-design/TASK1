ce84482b74c2e3311dab71f13768b283eaae7b2a
chintakarthik33-design <chintakarthik33@gmail.com>
2026-06-27 23:15:59 +0530

    first commit

---
+++ opeartors.py
@@ -0,0 +1,159 @@
+#types of operators
+# arithematic
+# assignment
+# relational&compression
+# logical
+# bitwise
+# membership
+# identity
+ 
+
+# x+y _ operator
+# += operator
+# x,y are operands
+
+# arithemetic _ mathematical operation betwen operands 
+# + - * / // % **
+# m=10
+# n=20
+# print(m+n)
+# print(m-n)
+# print(m*n)
+# print(m/n)
+# print(m//n)
+# print(m%n)
+# print(m**n)
+
+# swap 2 numbers without using 3 variable 
+# a,b= b,a
+# x=20
+# y=30
+# print(x,"",y) 
+# x=x+y 20+30=50
+# y=x-y  50-30=20
+# x=x-y  50-20=30
+# print(x," ",y)
+ 
+
+# assigenment operator
+# x=10
+# y=3
+# x+=y
+# print(x)
+
+# x-=y
+# print(x)
+
+# x*=y
+# print(x)
+
+# x/=y
+# print(x)
+# x//=y
+# print(x)
+# x%=y
+# print(x)
+# y**=x
+# print(y)
+
+# realtional and compression operator
+# compare two values return true or false
+# x=20
+# y=30
+# z=40
+# print(x<y)
+# print(x>y)
+# print(x>=y)
+# print(x<=y)
+# print(x==y)
+# print(x!=y)
+# print(x>y>Z)
+# print(x<y<z)
+# print(x>y<z)
+
+# logical operators
+# compare two or more condition
+# and or not 
+# and  if any condition false then full statement is false 
+
+# and
+# x=10
+# y=20
+# z=30
+# print((x<y) and (y<z))#true 
+# or  if any conditon true then fullstatement alose true 
+# print((x>y) and (y<z)) # false
+
+# not if any condition true then itrs false other any condition false that is true 
+# 1 0
+# 0 1
+# print(x<y)#true 
+# print( not(x<y))#false
+ 
+# bitwise operator 
+# 0 1
+# based on bits 
+# & and
+# | or
+# ^ xor
+# ~ not
+# << left shift
+# >> right shift
+#  return value 1 & 2
+# int1  int2  and  or  not 
+# o      0     0    0  0
+# 0      1     0    1  1
+# 1      0     0    1  1
+# 1      1     1    1  0
+# print(2&3)
+# print(10&5)
+# print(10|5)
+# print(7&1)
+# print(6&3)
+ 
+# find given values 2*n or not 
+
+# n=8
+# print(n&(n-1)==0)
+# print(n&(n+1)==0)
+# 2 swap two number without using 3rd var and a,b = b,a using bits 
+# a=5
+# b=10
+# print("before swapping ",'a=',a,'b=',b)
+# a=a^b
+# b=a^b
+# a=a^b
+# print("after swapping ",'a=',a,'b=',b)
+# a^=b
+# b^=a
+# a^=b
+# ~ not 
+# a = 10
+# print(~a)
+
+# << left shift
+# x = 10
+# print(x<<2)
+# print(x<<1)
+# print(x<<3)
+
+# >> right shift 
+# x=10
+# print(x>>2)
+# print(x>>3)
+# print(x>>3)
+# task 
+# a=10
+# b=10
+# print(a^b)
+# a=2
+# b=8
+# print(a&b)
+# a=3
+# b=7
+# print(a&b)
+
+
+
+
+
