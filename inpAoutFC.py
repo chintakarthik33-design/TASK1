be161f665c5cf9df46eec29af982cb357b497450
chintakarthik33-design <chintakarthik33@gmail.com>
2026-06-27 22:41:22 +0530

    first commit

---
+++ inpAoutFC.py
@@ -0,0 +1,19 @@
+
+#input function KeyError
+n = input("Enter number: ")
+print(n + 5)  # TypeError: can only concatenate str to int
+# value error
+n = int(input("Enter number: ")) # user "abc" enter cheste
+#eof error
+n = input()  # Online judge lo input ivvakapothe
+#
+
+#output function error
+#name error
+print(name)  # NameError: name 'name' is not defined
+#syntax error
+print("Hello)  # SyntaxError: unterminated string
+    
+#type error in print
+age = 20
+print("Age is " + age)  # TypeError: can only concatenate str to str
